69d20bb23c6b88f8e43a07572cf4d42237ca74d5
Ruslan Bilyshko <ruslan.ua@mail.ru>
2017-03-11 22:03:10 +0200

    Add other methods from change authors

--- views.py
+++ views.py
@@ -20,10 +20,47 @@ def create_author():
     return jsonify(author_schema.dump(author).data), 201
 
 
+@app.route('/api/authors/<int:id>', methods=["PUT"])
+def update_author(id):
+    try:
+        author = Author.get(id=id)
+    except Author.DoesNotExist:
+        return jsonify({"message": "Can't find author with id - `{id}`".format(id=id)}), 404
+
+    author, errors = author_schema.load(request.json, instance=author)
+
+    if errors:
+        return jsonify(errors), 400
+
+    author.save()
+
+    return jsonify(book_schema.dumps(author).data), 200
+
+
+@app.route('/api/authors/<int:id>', methods=["DELETE"])
+def delete_author(id):
+    is_author_exists = Author.select().filter(id=id).exists()
+
+    if not is_author_exists:
+        return jsonify({"message": "Can't find author with id - `{id}`".format(id=id)}), 404
+
+    Author.delete().where(Author.id == id).execute()
+    return jsonify({}), 204
+
+
 @app.route('/api/authors', methods=["GET"])
 def get_authors():
     authors = list(Author.select())
     return jsonify(author_schema.dump(authors, many=True).data)
+
+
+@app.route('/api/authors/<int:id>', methods=["GET"])
+def get_one_author(id):
+    try:
+        author = Author.get(id=id)
+        return jsonify(author_schema.dump(author).data)
+    except Author.DoesNotExist:
+        return jsonify({"message": "Can't find author with id - `{id}`".format(id=id)}), 404
 
 
 @app.route('/api/books', methods=["POST"])
@@ -68,6 +105,7 @@ def update_book(id):
 
     return jsonify(book_schema.dumps(book).data), 200
 
+
 @app.route('/api/books/<int:id>', methods=["DELETE"])
 def delete_book(id):
     is_book_exists = Book.select().filter(id=id).exists()
@@ -81,6 +119,6 @@ def delete_book(id):
 
 if __name__ == '__main__':
     initialize()
-    #app.debug = True
-    #app.run()
+    # app.debug = True
+    # app.run()
     app.run(use_reloader=True)
